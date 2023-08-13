3ade55ce27c98eaebcff78ad8dd64c7e66692b4f
Sunil Kumar bais <sunilkumarbais46@gmail.com>
2023-08-13 17:14:01 +0530

    MyAccount

---
+++ Script.js
@@ -0,0 +1,11 @@
+
+alert("Do you want to enter my page ? ");
+
+let massage = prompt("Do you want to see my page ? \n So input your age. " , "guest ");
+if(massage>=18){
+    console.log("You are eligible for see my page.");
+}
+else{
+    console.log("You are not eligible for see my page.");
+}
+console.log(massage);
